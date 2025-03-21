5fa1be4b9099bbc65a90813a61ebbeb3e92be771
yasickogul <yasickogul0705@gmail.com>
2025-03-21 21:03:32 +0530

    Initial commit

---
+++ app/src/main/java/com/example/bookshop/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.bookshop // Adjust the package name based on your project
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_get_start)
+
+        val startButton = findViewById<Button>(R.id.startButton)
+        startButton.setOnClickListener {
+            // Navigate to another screen or start an action here
+            // For now, we'll just display a log message
+            // Example: Navigate to another activity or screen
+        }
+    }
+}
