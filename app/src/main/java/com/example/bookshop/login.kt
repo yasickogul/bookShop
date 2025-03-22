c5c674e775bf915f13eb4e9d13422b475e64e5f2
yasickogul <yasickogul0705@gmail.com>
2025-03-23 00:52:54 +0530

    get start page,Register page: almostDone

--- app/src/main/java/com/example/bookshop/login.kt
+++ app/src/main/java/com/example/bookshop/login.kt
@@ -1,34 +1,20 @@
 package com.example.bookshop
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class GetStart : AppCompatActivity() {
+class login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_get_start)
-
-        // Set padding for system bars
+        setContentView(R.layout.activity_login)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        // Find the button by ID
-        val startButton: Button = findViewById(R.id.startButton)
-
-        // Set a click listener on the "Get Started" button
-        startButton.setOnClickListener {
-            // Start LoginActivity when the button is clicked
-            val intent = Intent(this, login::class.java)
-            startActivity(intent)
         }
     }
 }
